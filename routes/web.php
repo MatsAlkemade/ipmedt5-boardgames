9a71877f4d1a6465b3d2be82f0cba5a18f3c2337
MatsAlkemade <matsalkemade@gmail.com>
2021-03-28 17:05:48 +0200

    laravel breeze geinstalleerd

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::get('/games', [\App\Http\Controllers\GamesController::class, 'index']);
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
