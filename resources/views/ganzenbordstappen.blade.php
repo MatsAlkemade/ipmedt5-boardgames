fa34a253279408bcdf4f68063f1a405a52c8fab2
Isa <vuilnisbuiltje@gmail.com>
2021-03-31 14:31:28 +0200

    Imagess veel imagess en css

--- resources/views/ganzenbordstappen.blade.php
+++ resources/views/ganzenbordstappen.blade.php
@@ -1,12 +1,44 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<h2 >{{$ganzenbordstappen[0]->description}} </h2>
-</body>
-</html>
+@extends('default')
+
+@section('content')
+<h2 >Ganzenbord</h2>
+<ul class="gb__players">
+                <li class="gb__player">
+                    <figure class="gb__players__figure">
+                        <img src="/img/gb_vakjes/gb_start.png" alt="Start vakje">
+                        <figcaption>Speler 1</figcaption>
+                    </figure>
+                </li>
+                <li class="gb__player">
+                    <figure class="gb__players__figure">
+                     <img src="/img/gb_vakjes/gb_start.png" alt="Start vakje">
+                        <figcaption>Speler 2</figcaption>
+                    </figure>
+                </li>
+                <li class="gb__player">
+                    <figure class="gb__players__figure">
+                      <img src="/img/gb_vakjes/gb_start.png" alt="Start vakje">
+                        <figcaption>Speler 3</figcaption>
+                    </figure>
+                </li>
+                <li class="gb__player">
+                    <figure class="gb__players__figure">
+                        <img src="/img/gb_vakjes/gb_start.png" alt="Start vakje">
+                        <figcaption>Speler 4</figcaption>
+                    </figure>
+
+                </li>
+            </ul>
+
+
+
+
+
+
+
+
+
+
+
+
+@endsection
