05a680d39caa314b2278bfce6f956047f6b43b5a
Isa <vuilnisbuiltje@gmail.com>
2021-03-31 16:16:47 +0200

    css update

--- resources/views/ganzenbordstappen.blade.php
+++ resources/views/ganzenbordstappen.blade.php
@@ -1,30 +1,53 @@
 @extends('default')
 
 @section('content')
-<h2 >Ganzenbord</h2>
+<i class="fas fa-home gb__home"></i>
+<h2 class="gb__title" >Ganzenbord</h2>
 <ul class="gb__players">
+                <li class="gb__players__goose">
+                    <figure class="gb__players__goose__figure">
+                        <img src="/img/gb_vakjes/gb_rood.png" alt="gans">
+                        <figcaption>Speler 1</figcaption>
+                    </figure>
+                </li>
+                <li class="gb__players__goose">
+                    <figure class="gb__players__goose__figure">
+                     <img src="/img/gb_vakjes/gb_paars.png" alt="gans">
+                     <figcaption>Speler 1</figcaption>
+                    </figure>
+                </li>
+                <li class="gb__players__goose">
+                    <figure class="gb__players__goose__figure">
+                      <img src="/img/gb_vakjes/gb_geel.png" alt="gans">
+                      <figcaption>Speler 1</figcaption>
+                    </figure>
+                </li>
+                <li class="gb__players__goose">
+                    <figure class="gb__players__goose__figure">
+                        <img src="/img/gb_vakjes/gb_blauw.png" alt="gans">
+                        <figcaption>Speler 1</figcaption>
+                    </figure>
+                </li>  
+
+
                 <li class="gb__player">
                     <figure class="gb__players__figure">
                         <img src="/img/gb_vakjes/gb_start.png" alt="Start vakje">
-                        <figcaption>Speler 1</figcaption>
                     </figure>
                 </li>
                 <li class="gb__player">
                     <figure class="gb__players__figure">
                      <img src="/img/gb_vakjes/gb_start.png" alt="Start vakje">
-                        <figcaption>Speler 2</figcaption>
                     </figure>
                 </li>
                 <li class="gb__player">
                     <figure class="gb__players__figure">
                       <img src="/img/gb_vakjes/gb_start.png" alt="Start vakje">
-                        <figcaption>Speler 3</figcaption>
                     </figure>
                 </li>
                 <li class="gb__player">
                     <figure class="gb__players__figure">
                         <img src="/img/gb_vakjes/gb_start.png" alt="Start vakje">
-                        <figcaption>Speler 4</figcaption>
                     </figure>
 
                 </li>
