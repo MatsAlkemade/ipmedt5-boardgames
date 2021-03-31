6c22c6f6f989bc53083e788c5c877297db3517c4
Kasper <s1118202@student.hsleiden.nl>
2021-03-31 11:18:31 +0200

    landings pagina voor trivial pursuit

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [\App\Http\Controllers\GamesController::class, 'index']);
+Route::get('/trivialpursuit', [\App\Http\Controllers\TrivialPursuitController::class, 'index']);
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
