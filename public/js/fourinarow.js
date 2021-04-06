28a0e7501521eb20c06d5dc3ce2994fa588b488b
Roy Oosterlee <roy@famoosterlee.nl>
2021-04-06 13:57:32 +0200

    Updated fourinarow.js

--- public/js/fourinarow.js
+++ public/js/fourinarow.js
@@ -1,5 +1,10 @@
 const socket = io(window.location.protocol + '//' + window.location.host, { transports: ['websocket'] });
 
+window.addEventListener('load', function() {
+	const fiar = document.querySelector('.fourinarow');
+	fiar.style.display = "none";
+});
+
 socket.on('connect', function() {
 	console.log("Connected to socketio server!");
 
@@ -17,3 +22,76 @@ socket.on('users', function(data) {
 
 	document.querySelector('.js--users').innerText = JSON.stringify(data);
 });
+
+socket.on('game_start', function(data) {
+	const fiar = document.querySelector('.fourinarow');
+	fiar.style.display = "block";
+	fiarBuilder();
+});
+
+let players = 2;
+function fiarBuilder() {
+	const fiar = document.querySelector('.fourinarow__pieces');
+	let r = -1;
+	for (let i = 0; i < 8*8; i++) {
+		r += (i % 8 == 0) ? 1 : 0;
+		const d = document.createElement('div');
+		d.classList.add('fourinarow__piece')
+		d.setAttribute("data-row", r);
+		d.setAttribute("data-column", i%8);
+		d.style.setProperty("--piece-row", r);
+		d.style.setProperty("--piece-column", i%8);
+		fiar.appendChild(d);
+	}
+
+	setButtons();
+}
+
+function setButtons() {
+	const buttons = document.querySelector('.fourinarow__buttons').children;
+
+	for (let i = 0; i < buttons.length; i++) {
+		buttons[i].addEventListener('click', place.bind(buttons[i], i, -1));
+	}
+}
+
+function place(column, player=0) {
+	const lic = getLastInColumn(column);
+	lic.classList.add("piece");
+	if (player == -1) {
+		// Player is this user
+		player = 1;
+	}
+	lic.classList.add("p" + player);
+	lic.style.setProperty("--piece-row", 0);
+	if (getLastInColumn(column) == null) return document.querySelector('.fourinarow__buttons > button:nth-child('+(column+1)+')').disabled = 'disabled';
+	checkWin();
+}
+
+function getLastInColumn(column) {
+	const fiar = document.querySelector('.fourinarow__pieces');
+	for (let i = column; i < fiar.children.length; i+=8) {
+		const f = fiar.children[i];
+		// console.log(f.classList.contains("piece"), f);
+		if (f.classList.contains("piece")) {
+			// console.log(column, ((i-column)/8)-1, i);
+			return document.querySelector("[data-column='"+column+"'][data-row='"+String(((i-column)/8)-1)+"']");
+		}
+	}
+	return document.querySelector("[data-column='"+column+"'][data-row='7']");
+}
+
+function checkWin() {
+	for (let i = 0; i < players; i++) {
+		const pp = document.querySelectorAll('.fourinarow__pieces > .p' + (i+1));
+		// console.log(pp);
+		if (pp == null || pp.length == 0) continue;
+
+		for (let i = 0; i < pp.length; i++) {
+
+			// console.log(pp[i]);
+		}
+	}
+
+	return false;
+}
--- resources/views/games/fourinarow.blade.php
+++ resources/views/games/fourinarow.blade.php
@@ -1,15 +1,27 @@
 @extends('games.defaultGame')
 @section('head-extra')
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 <script src="/js/fourinarow.js"></script>
+<link rel="stylesheet" type="text/css" href="/css/fourinarow.css">
 @endsection
 
 @section('title')
     Vier op een rij
 @endsection
 @section('gamecontent')
-    <article class="defaultGame__main">
-        <h1>Four in a row</h1>
-        <p>Join code: <span>{{ $gameCode }}</span></p>
-        <p class="js--users">{{ $users }}</p>
-    </article>
+    <section class="fourinarow">
+		<section class="fourinarow__buttons">
+			<button class="fourinarow__button"><i class="fad fa-arrow-square-down"></i></button>
+			<button class="fourinarow__button"><i class="fad fa-arrow-square-down"></i></button>
+			<button class="fourinarow__button"><i class="fad fa-arrow-square-down"></i></button>
+			<button class="fourinarow__button"><i class="fad fa-arrow-square-down"></i></button>
+			<button class="fourinarow__button"><i class="fad fa-arrow-square-down"></i></button>
+			<button class="fourinarow__button"><i class="fad fa-arrow-square-down"></i></button>
+			<button class="fourinarow__button"><i class="fad fa-arrow-square-down"></i></button>
+			<button class="fourinarow__button"><i class="fad fa-arrow-square-down"></i></button>
+		</section>
+		<section class="fourinarow__pieces">
+			
+		</section>
+	</section>
 @endsection
