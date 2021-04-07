598bc493c3debfb3145b9cd2ca3ea89e412be2e7
Roy Oosterlee <roy@famoosterlee.nl>
2021-04-07 13:38:53 +0200

    Fix

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"> 
-    <link rel="stylesheet" href="//css/main.css">
+    <link rel="stylesheet" href="/css/main.css">
     <link rel="stylesheet" type="text/css" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.4.0/socket.io.min.js"></script>
     @yield('head-extra')
-    <script src="//js/main.js" defer></script>
+    <script src="/js/main.js" defer></script>
     <title>@yield('title')</title>
 </head>
 <body>
