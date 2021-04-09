85d5487f9dd27e8b7014e7b04dcb3046dcd95cff
Roy Oosterlee <roy@famoosterlee.nl>
2021-04-09 23:38:58 +0200

    Fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -37,5 +37,8 @@ class AppServiceProvider extends ServiceProvider
 
             return $output;
         });
+        if (env("APP_SSL")) {
+            $this->app['request']->server->set('HTTPS','on');
+        }
     }
 }
