a3e2006b16b289e7f0cb26b04f09bb20c4940c26
Kasper <s1118202@student.hsleiden.nl>
2021-04-12 13:41:51 +0200

    spelregels

--- resources/views/games/trivialpursuit.blade.php
+++ resources/views/games/trivialpursuit.blade.php
@@ -12,3 +12,22 @@
 </script>
 <script src="/js/trivialpursuit.js"></script>
 @endsection
+
+@section('gamecontent')
+
+@endsection
+
+@section('livechat')
+
+@endsection
+
+@section('rules')
+<p>Het doel van het spel is om als eerst de taart te vullen. Je kan taart stukjes verdienen door op de speciale vakjes te landen (de gele rand) en de vraag goed te beantwoorden.</p>
+<p><br></p>
+<p>Na dat je gegooid hebt landt je op vakje en komt er een vraag op het scherm van die caetgorie, als je de vraag goed hebt mag je nog een keer gooien heb je de vraag fout dan is de volgende speler aan de beurt</p>
+<p><br></p>
+<p>Als een speler op een donker blauw vakje komt hoeft hij geen vraag te beantwoorden, maar mag hij nog een keer gooien.</p>
+@endsection
+
+
+
