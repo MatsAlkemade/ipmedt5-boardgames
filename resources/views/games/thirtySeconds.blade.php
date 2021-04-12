8e9a0a43ec6ae77933f1be21a16f227efe5b5de7
MatsAlkemade <matsalkemade@gmail.com>
2021-04-12 11:42:55 +0200

    aparte js en css voor thirtyseconds

--- resources/views/games/thirtySeconds.blade.php
+++ resources/views/games/thirtySeconds.blade.php
@@ -3,6 +3,17 @@
     30 Seconds
 @endsection
 
+@section('head-extra')
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+<script type="text/javascript">
+	let __u__ = "{{ auth()->user()->email }}";
+	let __p__ = "{{ auth()->user()->password }}";
+	let user_id = {{ auth()->user()->id }};
+</script>
+<script src="/js/thirtyseconds.js"></script>
+<link rel="stylesheet" type="text/css" href="/css/thirtyseconds.css">
+@endsection
+
 <?php
     $r = mt_rand(0,1);
 ?>
